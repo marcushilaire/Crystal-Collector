7578024a9130e63e64dbf3a6bcd549d62fface64
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-02-12 13:20:59 -0500

    linked jquery and set up an looping function to create the arrays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,19 @@
-console.log("linktest");
+var goal = 0;
+var currentNumber = 0;
+var crystalA = 0;
+var crystalB = 0;
+var crystalC = 0;
+var crystalD = 0;
+var wins = 0;
+var losses = 0;
+var crystalNumbers = []; //1 through 12
+var goalNumbers = []; //19 through 120
+// setting up crystal number array
+for (i=1; i<13; i++){
+    crystalNumbers.push(i);
+};
+for (i=19; i<121; i++){
+    goalNumbers.push(i);
+};
+console.log(crystalNumbers);
+console.log(goalNumbers);
