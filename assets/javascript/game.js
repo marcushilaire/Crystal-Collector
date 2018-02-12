e8e3b51feee90401207e7da45f5e9a2dc92d714c
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-02-12 13:41:51 -0500

    added play button and wrote randomizing functions for crystal values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// Declaring variables
 var goal = 0;
 var currentNumber = 0;
 var crystalA = 0;
@@ -6,14 +7,36 @@ var crystalC = 0;
 var crystalD = 0;
 var wins = 0;
 var losses = 0;
-var crystalNumbers = []; //1 through 12
-var goalNumbers = []; //19 through 120
+var crystalNumbers = []; // numbers 1 through 12 will be pushed to this array
+var goalNumbers = []; // numbers 19 through 120 will be pushed to this array
+// functions below
+
 // setting up crystal number array
 for (i=1; i<13; i++){
     crystalNumbers.push(i);
-};
+}
+
+console.log(" 1 through 12" + crystalNumbers)
+console.log(" 19 through 120" +goalNumbers)
+
+
 for (i=19; i<121; i++){
     goalNumbers.push(i);
-};
-console.log(crystalNumbers);
-console.log(goalNumbers);
+}
+
+var randomCrystalA = function() {
+    crystalA = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
+    console.log(crystalA);
+}
+var randomCrystalB = function() {
+    crystalB = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
+    console.log(crystalB);
+}
+var randomCrystalC = function () {
+    crystalC = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
+    console.log(crystalC);
+}
+var randomCrystalD = function () {
+    crystalD = crystalNumbers[Math.floor(Math.random() * crystalNumbers.length)];
+    console.log(crystalD);
+}
